79f244e414d1064cc6888490a4e031929dcadbd6
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-29 11:40:24 +0900

    LeetCode 125번 Valid Palindrome 예시 답안 코드 수정

--- src/leetcode/reference/ValidPalindrome.java
+++ src/leetcode/reference/ValidPalindrome.java
@@ -1,26 +1,16 @@
 package leetcode.reference;
 
-import java.io.*;
-
 public class ValidPalindrome {
-    public static void main(String[] args) throws IOException {
+	public boolean isPalindrome(String s) {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		// 정규식으로 유효한 문자만 추출한 뒤 모두 소문자로 변경
+		String filteredString = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        System.out.println(isPalindrome(br.readLine()));
-    }
+		// 문자열을 뒤집은 뒤 `String`으로 변경
+		String reversedString = new StringBuilder(filteredString).reverse().toString();
 
-    public static boolean isPalindrome(String s) {
+		// 두 문자열 비교
+		return filteredString.equals(reversedString);
 
-        // 정규식으로 유효한 문자만 추출한 뒤 모두 소문자로 변경
-        String filteredString = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-        // 문자열을 뒤집은 뒤 `String`으로 변경
-        String reversedString = new StringBuilder(filteredString).reverse().toString();
-
-        // 두 문자열 비교
-        return filteredString.equals(reversedString);
-
-    }
+	}
 }
